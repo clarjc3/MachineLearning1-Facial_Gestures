9175601be3e72406525f2745e9593d70fe87e3e5
clarjc3 <clarjc3@student.op.ac.nz>
2019-06-19 16:07:05 +1200

    Removing unused old code

--- FRON.py
+++ FRON.py
@@ -31,62 +31,7 @@ class App(QWidget):
 		
 		self.initUI()
 		
-		# using the landmarks method
-		self.landmarks()
-	
-	def webcam(self):
-		cap = cv2.VideoCapture(0)
-
-		while(True):
-			# Capture frame-by-frame
-			ret, frame = cap.read()
-
-			# Our operations on the frame come here
-			gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-			# Display the resulting frame
-			cv2.imshow('hey', gray)
-			if cv2.waitKey(1) & 0xFF == ord('q'):
-				break
-
-		# When everything done, release the capture
-		cap.release()
-		cv2.destroyAllWindows()
-	
-	def Face(self):
-		cascPath = sys.argv[1]
-		faceCascade = cv2.CascadeClassifier(cascPath)
-		video_capture = cv2.VideoCapture(0)
-		while True:
-			# Capture frame-by-frame
-			ret, frame = video_capture.read()
-
-			gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-			faces = faceCascade.detectMultiScale(
-				gray,
-				scaleFactor = 1.1,
-				minNeighbors = 5,
-				minSize = (30, 30),
-				# flags = cv2.cv.CV_HAAR_SCALE_IMAGE
-				flags = cv2.CASCADE_SCALE_IMAGE
-			)
-
-			# Draw a rectangle around the faces
-			for (x, y, w, h) in faces:
-				cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-
-			# Display the resulting frame
-			cv2.imshow('Video', frame)
-
-			if cv2.waitKey(1) & 0xFF == ord('q'):
-				break
-
-		# When everything is done, release the capture
-		video_capture.release()
-		cv2.destroyAllWindows()
-	
-	
+		self.landmarks() # Using the landmarks method
 
 	def landmarks(self):
 		# let's go code an faces detector(HOG) and after detect the 
